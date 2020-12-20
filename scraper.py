59b22736004745cacc87747c363dde514a0ae391
Muhammad Syukri Zainal <msyukri.zaidin@gmail.com>
2020-12-20 13:11:33 +0800

    Remove parallel packages

--- scraper.py
+++ scraper.py
@@ -10,7 +10,6 @@ import csv
 import os
 from datetime import datetime
 import re
-from joblib import Parallel, delayed, parallel_backend
 import sys
 
 class House:
@@ -18,56 +17,68 @@ class House:
         self.num_bedroom = 'NA'
         self.num_bathroom = 'NA'
         self.num_garage = 'NA'
-        self.floor_area = 'NA'
+        #self.floor_area = 'NA'
         self.land_area = 'NA'
-        self.building_type = 'NA'
-        self.yr_built = 'NA'
-        self.primary_use = 'NA'
+        #self.building_type = 'NA'
+        #self.yr_built = 'NA'
+        #self.primary_use = 'NA'
         self.last_sold_date = 'NA'
         self.last_sold_price = 'NA'
         self.land_price = 'NA'
-        self.nearest_pri = 'NA'
-        self.nearest_pri_dist = 'NA'
-        self.nearest_sec = 'NA'
-        self.nearest_sec_dist = 'NA'
+        #self.nearest_pri = 'NA'
+        #self.nearest_pri_dist = 'NA'
+        #self.nearest_sec = 'NA'
+        #self.nearest_sec_dist = 'NA'
 
     def toList(self):
-        attribute_list = [self.num_bedroom, self.num_bathroom, self.num_garage, 
-                        self.floor_area, self.land_area, self.building_type, self.yr_built, self.primary_use,
-                        self.last_sold_date, self.last_sold_price, self.land_price, self.nearest_pri, self.nearest_pri_dist,
-                        self.nearest_sec, self.nearest_sec_dist]
+        attribute_list = [self.num_bedroom, self.num_bathroom, self.num_garage, self.land_area,
+                        self.last_sold_date, self.last_sold_price, self.land_price]
         return attribute_list
+
+    def printAll(self):
+        print("Bedrooms: ", self.num_bedroom)
+        print("Bathrooms: ", self.num_bathroom)
+        print("Garages: ", self.num_garage)
+        print("Land Area: ", self.land_area)
+        print("Last Sold Date: ", self.last_sold_date)
+        print("Last Sold Price: ", self.last_sold_price)
+        print("Land Price: ", self.land_price)
     
-def check_proxy(process_index):
+def check_proxy():
     options = webdriver.ChromeOptions()
 
-    #Residential Proxies
-    PROXY = 'au.smartproxy.com:30000'
+    #IP Authenticated Proxies
+    PROXY = 'au.smartproxy.com:30000' #Residential
+    #PROXY = '23.250.83.82:80' #Datacenter
     options.add_argument('--proxy-server=%s' % PROXY)
     options.add_argument('--disable-extensions')
 
-    #Datacenter Proxies
-    #options.add_extension("proxy.zip")
+    #User/PW Authenticated Proxies
+    #options.add_extension("proxy.zip") #Residential
 
     options.add_argument('start-maximized')
     prefs={"profile.managed_default_content_settings.images": 2}
     options.add_experimental_option('prefs', prefs)
     prefs = {'disk-cache-size': 4096}
     options.add_experimental_option('prefs', prefs)
-    options.add_argument("user-data-dir=C:/Selenium_profile/User Data " + str(process_index))
-    options.add_argument("profile-directory=Profile 1")
+    #options.add_argument("user-data-dir=C:/Selenium_profile/User Data " + str(process_index))
+    #options.add_argument("profile-directory=Profile 1")
 
+    #ua = UserAgent()
+    #userAgent = ua.random
     #options.add_argument(f'user-agent={userAgent}')
 
     driver = webdriver.Chrome(executable_path='chromedriver.exe', options=options)
     return driver
 
 def search_func(search_term_data, driver):
-    url = 'https://www.onthehouse.com.au/real-estate/wa/'
+    #url = 'https://www.onthehouse.com.au/real-estate/wa/'
+    url = 'https://www.realestateview.com.au/property-360/property/'
+    #url = 'https://www.domain.com.au/'
     search_term_data = [s.lower() for s in search_term_data]
-    search_term_data[3].replace(' ', '-') #If suburb is 2 words or more long, put a dash inbetween
-    search_term_data[1].replace(' ', '-') #If street is 2 words or more long, put a dash inbetween
-    url += search_term_data[3] + '-' + search_term_data[5] + '/' + search_term_data[1] + '-' + search_term_data[2] + '?' + 'streetNumber=' + search_term_data[0]
+    search_term_data[3] = search_term_data[3].replace(' ', '-') #If suburb is 2 words or more long, put a dash inbetween
+    search_term_data[1] = search_term_data[1].replace(' ', '-') #If street is 2 words or more long, put a dash inbetween
+    url += search_term_data[0] + '-' + search_term_data[1] + '-' + search_term_data[2] + '-' + search_term_data[3] + '-' + search_term_data[4] + '-' + search_term_data[5] 
     try:
         driver.get(url)
     except: #URL does not exist
@@ -75,90 +86,42 @@ def search_func(search_term_data, driver):
         
     #Wait for new page to load then click the card link that appears
     try:
-        card_link = WebDriverWait(driver, 60).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'PropertySearch__resultSlot--1YH_u'))
+        WebDriverWait(driver, 60).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'pe-bbc-size__item'))
         )
-    except TimeoutException:
+    except:
         return 0
 
-    card_link.click()
     return 1
+
+def hasNumbers(inputString):
+    return bool(re.search(r'\d', inputString))
 
 #Basic details present in all houses
 def get_basic_details(obj, driver):
-    obj.num_bedroom = driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div[2]/div/div[1]/div/div[1]/div[2]/div[1]/div[2]').text
-    obj.num_bathroom = driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div[2]/div/div[1]/div/div[1]/div[2]/div[2]/div[2]').text
-    obj.num_garage = driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div[2]/div/div[1]/div/div[1]/div[2]/div[3]/div[2]').text
-
+    obj.num_bedroom = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[2]/div[1]/div/div/div[2]/div/div[1]/span[1]/span').text
+    obj.num_bathroom = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[2]/div[1]/div/div/div[2]/div/div[1]/span[2]/span').text
+    obj.num_garage = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[2]/div[1]/div/div/div[2]/div/div[1]/span[3]/span').text
+    obj.land_area = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[2]/div[1]/div/div/div[2]/div/div[1]/span[4]').text
 #Details which may or may not be present
 def get_intermediate_details(obj, driver):
-    property_details = driver.find_elements_by_xpath("//li[contains(@class, 'pb-2') and contains(@class, 'mb-2')]")
-    for i in range(len(property_details)):
-        p = property_details[i].text.split('\n') #Each item in property details has its value and description separated by \n
-        if p[0] == 'Building Type':
-            obj.building_type = p[1]
-        elif p[0] == 'Year Built':
-            obj.yr_built = p[1]
-        elif p[0] == 'Floor Size':
-            obj.floor_area = re.sub('\D\w+', '', p[1])
-        elif p[0] == 'Land Size':
-            obj.land_area = re.sub('\D\w+', '', p[1])
-        elif p[0] == 'Primary Land Use':
-            obj.primary_use = p[1]
-        else:
-            continue
+    history_list = driver.find_elements_by_xpath("//div[contains(@class, '_table-row')]")
+    sale_history = []
+    for item in history_list:
+        if "Sold" in item.text:
+            s = item.text.split() #E.g May 2016 Sold $335,000 would be ['May', '2016', 'Sold', '$335,000']
+            sale_history.append(s)
 
-#Gets history. May or may not be present
-def get_history(obj, driver):
-    try:
-        history_text = driver.find_element_by_xpath("//div[contains(@class, 'mb-4') and contains(@class, 'pb-4') and contains(@class, 'PropertyHistory__latestEvent--2QiUe')]")
-        #print("there is history")
-    except:
-        #print("no history")
-        return
-
-    history_list = driver.find_elements_by_xpath("//div[contains(@class, 'flex-column') and contains(@class, 'w-50')]//*")
-
-    try:
-        obj.last_sold_date = history_list[2].text
-        last_sold_obj = datetime.strptime(obj.last_sold_date, '%d %b %Y')
-    except IndexError:
-        return
-
-    try:
-        int(obj.yr_built)
-    except ValueError:
-        #No year built data was found
-        return
-
-    if int(obj.yr_built) >= last_sold_obj.year:
-        #if house built after it was sold, then the price stated is the land price
-        obj.land_price = history_list[1].text.strip('$k')
+    if len(sale_history) == 1: #history_list[0] would be considered land price
+        if hasNumbers(sale_history[0][3]):
+            obj.land_price = sale_history[0][3]
+        obj.last_sold_date = sale_history[0][0] + ' ' + sale_history[0][1]
+    elif len(sale_history) >= 1:
+        if hasNumbers(sale_history[0][3]):
+            obj.last_sold_price = sale_history[0][3]
+        obj.last_sold_date = sale_history[0][0] + ' ' + sale_history[0][1]
     else:
-        obj.last_sold_price = history_list[1].text.strip('$k')
-
-#Gets schools by proximity. Almost always present but have to get the nearest
-def get_schools(obj, driver):
-    try:
-        school_list = driver.find_elements_by_xpath("//div[contains(@class, 'Explore__schoolRow--3TE8e')]")
-    except NoSuchElementException:
-        #No school list
         return
-
-    for i in range(len(school_list)):
-        s = school_list[i].text.split('\n')
-        school = s[0]
-        distance = s[1]
-        attributes = s[3]
-        if 'PRIMARY' in attributes and obj.nearest_pri_dist == 'NA':
-            obj.nearest_pri = school
-            obj.nearest_pri_dist = distance
-        elif 'SECONDARY' in attributes and obj.nearest_sec_dist == 'NA':
-            obj.nearest_sec = school
-            obj.nearest_sec_dist = distance
-        elif attributes.count('COMBINED') >= 2 and obj.nearest_sec_dist == 'NA':
-            obj.nearest_sec = school
-            obj.nearest_sec_dist = distance
 
 #Opens a file for appending
 def write_data(h1, search_term_data, additional_data, file_name):
@@ -174,112 +137,26 @@ def skipped(search_term_data, driver):
         f.write(' '.join(e.strip('\r') for e in search_term_data) + '\r\n')
         driver.close()
 
-def scrape(search_term_data, additional_data, file_name, process_index):
+def scrape(search_term_data):
     #Initialisation of driver
-    driver = check_proxy(process_index)
+    driver = check_proxy()
 
     #Better error handling here?
     if search_func(search_term_data, driver) == 0: #If search failed
-        skipped(search_term_data, driver)
-        return
-
-    #Wait for new page to appear
-    try:
-        WebDriverWait(driver, 60).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'smText'))
-        )
-    except:#Timeout
-        skipped(search_term_data, driver)
-        return 
+        return None
 
     h1 = House() #House object
     try:
         get_basic_details(h1, driver)
     except:
-        skipped(search_term_data, driver)
-        return
+        return None
+
     try:
         get_intermediate_details(h1, driver)
     except:
         pass
-    
-    get_history(h1, driver)
 
-    #Wait for 'nearby schools' section to appear
-    try:
-        WebDriverWait(driver, 60).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'Explore__schoolRow--3TE8e'))
-        )
-        get_schools(h1, driver)
-    except: #Timeout
-        pass
-    
-    write_data(h1, search_term_data, additional_data, file_name)
+    h1.printAll()
+    #write_data(h1, search_term_data, additional_data, file_name)
     driver.close()
-
-def get_addresses(file_name, process_index):
-    with open(file_name, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        for line_number, line in enumerate(reader):
-            if process_index == 1:
-                print("On line: ", line_number)
-            if line_number == 0: #Ignore header
-                continue
-            else:
-                search_term_data = line[:-2]
-                additional_data = line[-2:]
-                scrape(search_term_data, additional_data, file_name, process_index)
-                #scrape()
-                #write to another file
-
-def manual_proxy_check(num):
-    options = webdriver.ChromeOptions()
-
-    #IP Authenticated Proxies
-    #PROXY = 'au.smartproxy.com:30000' #Residential
-    #PROXY = '23.250.83.82:80' #Datacenter
-    #options.add_argument('--proxy-server=%s' % PROXY)
-    #options.add_argument('--disable-extensions')
-
-    #User/PW Authenticated Proxies
-    options.add_extension("proxy.zip") #Residential
-    
-
-    options.add_argument("user-data-dir=C:/Selenium_profile/User Data " + str(num))
-    options.add_argument("profile-directory=Profile 1")
-    options.add_argument('start-maximized')
-    driver = webdriver.Chrome(executable_path='chromedriver.exe', options=options)
-    #url = 'https://www.realestateview.com.au/'
-    #url = 'https://www.domain.com.au/'
-    #url = 'https://www.onthehouse.com.au'
-    url = 'http://lumtest.com/myip.json'
-    driver.get(url)
-    time.sleep(5)
-    return
-    
-#Could possibly use getopt to improve
-def main(argv):
-    if argv[0] == '-p' and len(argv) == 2:
-        #Check proxy
-        Parallel(n_jobs=int(argv[1]), prefer="threads")(delayed(manual_proxy_check)(i) for i in range(1, int(argv[1]) + 1))
-        return
-    if len(argv) < 2:
-        print("ERROR: No arguments\nUsage: python scraper.py [file name] [number of processor(s)]")
-        return
-    elif len(argv) > 2:
-        print("ERROR: Too many arguments\nUsage: python scraper.py [file name] [number of processor(s)]")
-        return
-
-    file_name = argv[0]
-    num_cores = int(argv[1])
-
-    CWD = os.getcwd()
-    files = []
-    CWD += '/address_data/'
-    for i in range(1, num_cores + 1):
-        files.append(CWD + file_name + '_part_' + str(i) + '.csv')
-
-    Parallel(n_jobs=num_cores, prefer="threads")(delayed(get_addresses)(file_name, process_index) for process_index, file_name in enumerate(files, 1))
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
+    return data
